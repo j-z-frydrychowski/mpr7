18c1981d83bd3b0a64332af3abdcb90f822c1200
j-z-frydrychowski <s32713@pjwstk.edu.pl>
2025-12-09 09:58:30 +0100

    pierwszy commit - przygotowanie projektu

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "com.test"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.18.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
